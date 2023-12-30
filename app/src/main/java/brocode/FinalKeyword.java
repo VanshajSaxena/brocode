0d8a491a3be8632d9fdb28647a580848a9abea32
Vanshaj Saxena <vs110405@outlook.com>
2023-12-30 21:12:48 +0530

    brocode(finalkeyword): Till final keyword timestamp 2:52:06
    the next commit will feature the opp section of the brocode course

---
+++ app/src/main/java/brocode/FinalKeyword.java
@@ -0,0 +1,18 @@
+package brocode;
+
+/**
+ * FinalKeyword
+ */
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+
+		final double pi=3.14159;
+
+		//pi = 4;
+
+		System.out.println(pi);
+		
+		
+	}
+}
--- app/src/main/java/brocode/Printf.java
+++ app/src/main/java/brocode/Printf.java
@@ -9,29 +9,29 @@ public class Printf {
 
 		boolean bol = true;
 		char character = '.';
-		String hello = "Hello";
-		int hmm = 123;
+		String name = "Arastu";
+		int number = 123;
 		double pi = 3.14;
 
 		// % [flags] [precision] [width] [conversion-character]
 
 		System.out.printf("%b ", bol);
 		System.out.printf("%c ", character);
-		System.out.printf("%s ", hello);
-		System.out.printf("%d ", hmm);
+		System.out.printf("%s ", name);
+		System.out.printf("%d ", number);
 		System.out.printf("%f ", pi);
 
 		System.out.println();
-		System.out.printf("Vanshaj %-30s ", hello + ".");
+		System.out.printf("Vanshaj %-30s ", name + ".");
 		System.out.printf("%c ", character);
-		System.out.printf("%s ", hello);
-		System.out.printf("%d ", hmm);
+		System.out.printf("%s ", name);
+		System.out.printf("%d ", number);
 
 		System.out.println("\r");
 		System.out.println("\r");
 
 		System.out.printf("The value of pi is: %f\r", pi);
-		System.out.printf("The value of pi is: %-20f\r", pi);
+		System.out.printf("The value of pi is%-20f\r", pi);
 		System.out.printf("The value of pi is: %+20f\r", pi);
 		System.out.printf("The value of pi is: %1.2f\r", pi);
 		System.out.printf("The value of pi is: %20f\r", pi);
